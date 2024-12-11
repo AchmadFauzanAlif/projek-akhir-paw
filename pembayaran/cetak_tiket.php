8d34441a1587ff26240084df643e9f61fb03a643
Achmad Fauzan Alif Fitrah <fznfitrah@gmail.com>
2024-12-11 23:42:38 +0700

    perbaikan cetak tiket

--- pembayaran/cetak_tiket.php
+++ pembayaran/cetak_tiket.php
@@ -4,11 +4,11 @@ session_start();
 include "../db.php";
 include "../function.php";
 
-
-
 // Ambil data tiket berdasarkan ID pemesanan yang diteruskan via GET
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
+    
+    // Query untuk mengambil data pemesanan
     $query = "SELECT p.id, p.jumlah_tiket, p.waktu_transaksi, p.tipe_tiket, pel.nama, pel.telp 
                 FROM pemesanan p
                 INNER JOIN pelanggan pel ON p.pelanggan_id = pel.id
@@ -21,6 +21,10 @@ if (isset($_GET['id'])) {
     }
 
     $tiket = $data_tiket[0];
+
+    // Query untuk mengambil detail tiket berdasarkan transaksi_id
+    $query_detail = "SELECT * FROM detail_tiket WHERE transaksi_id = $id";
+    $data_detail_tiket = query($query_detail);
 } else {
     echo "ID tiket tidak ditemukan.";
     exit;
@@ -39,21 +43,29 @@ if (isset($_GET['id'])) {
 </head>
 
 <body>
-    <div class="ticket">
-        <h3><img src="../img/logoGili.png" alt="logoGili" height="40px">Wisata Gili Labak</h3>
-        <p><strong>Nama:</strong> <?= $tiket['nama']; ?></p>
-        <p><strong>No. Telepon:</strong> <?= $tiket['telp']; ?></p>
-        <p><strong>Jumlah Tiket:</strong> <?= $tiket['jumlah_tiket']; ?></p>
-        <p><strong>Tanggal Booking:</strong> <?= $tiket['waktu_transaksi']; ?></p>
-        <p><strong>Tipe Tiket:</strong> <?= $tiket['tipe_tiket']; ?></p>
 
-<!--     Diki     -->
-        <div class="qr-code">
-            <img src="../img/barcode.png $tiket['id']; ?>" alt="QR Code">
-        </div>
+    <?php if (!empty($data_detail_tiket)): ?>
+        <?php foreach ($data_detail_tiket as $tiket_detail): ?>
+            <div class="ticket">
+                <h3><img src="../img/logoGili.png" alt="logoGili" height="40px">Wisata Gili Labak</h3>
+                <p><strong>Nama:</strong> <?= $tiket_detail['nama_pelanggan']; ?></p>
+                <p><strong>No. Telepon:</strong> <?= $tiket_detail['telp']; ?></p>
+                <p><strong>Jumlah Tiket:</strong> <?= $tiket['jumlah_tiket']; ?></p>
+                <p><strong>Tanggal Booking:</strong> <?= $tiket['waktu_transaksi']; ?></p>
+                <p><strong>Tipe Tiket:</strong> <?= $tiket['tipe_tiket']; ?></p>
 
-        <p style="text-align: center;">Kode Tiket: <?= uniqid("TKT-"); ?></p>
-    </div>
+                <!-- <div class="qr-code">
+                    <img src="../img/barcode.png" alt="QR Code">
+                </div> -->
+
+                <p style="text-align: center;">Kode Tiket: <?= uniqid("TKT-"); ?></p>
+                <hr> <!-- Garis pemisah antar tiket -->
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>Tidak ada detail tiket untuk transaksi ini.</p>
+    <?php endif; ?>
+
     <script>
         window.print();
     </script>
